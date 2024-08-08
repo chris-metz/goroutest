67dd0c5ee5fe84befbfa77659b6fd3978dea2d92
Chris <hello@chrismetz.xyz>
2024-08-08 12:23:11 +0200

    bump

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func worker() {
+	time.Sleep(1 * time.Second)
+	fmt.Println("worker runned")
+}
 
 func main() {
-	fmt.Println("yo hello")
+	var wg sync.WaitGroup
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			worker()
+		}()
+	}
+
+	wg.Wait()
 }
